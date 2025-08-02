50166b45bca2a0112f9c8ae2c3ec4a0380b807c1
gitfaizankhan <faizankhaninfo9@gmail.com>
2025-08-02 06:24:17 +0530

    bug fixed

--- src/app.js
+++ src/app.js
@@ -50,7 +50,6 @@ strategy(app);
 // Routes
 import userRouter from "./routes/user.routes.js";
 import transcribe from "./routes/transcribe.routes.js";
-import errorHandler from "./utils/errorHandler.js";
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/audio", transcribe);
